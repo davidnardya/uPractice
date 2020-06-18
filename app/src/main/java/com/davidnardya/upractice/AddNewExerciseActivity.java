bca4036bb99d1fe0f54e3eda3a5c00b52d119b7f
davidnardya@gmail.com <`1q`1q~!Q>
2020-06-18 18:41:23 +0300

    Added new plan and exercise activities

---
+++ app/src/main/java/com/davidnardya/upractice/AddNewExerciseActivity.java
@@ -0,0 +1,14 @@
+package com.davidnardya.upractice;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AddNewExerciseActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_new_exercise);
+    }
+}
---
+++ app/src/main/java/com/davidnardya/upractice/AddNewPlanActivity.java
@@ -0,0 +1,14 @@
+package com.davidnardya.upractice;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AddNewPlanActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_new_plan);
+    }
+}
