0df12086a7461a759f5b9d5560ae3355c5d74a55
davidnardya@gmail.com <`1q`1q~!Q>
2020-06-19 14:36:13 +0300

    Completed main adapter and recycler view, need to solve the Factory error on the main activity

--- app/src/main/java/com/davidnardya/upractice/activities/MainActivity.java
+++ app/src/main/java/com/davidnardya/upractice/activities/MainActivity.java
@@ -1,6 +1,9 @@
 package com.davidnardya.upractice.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
@@ -10,16 +13,22 @@ import android.widget.Button;
 import android.widget.TextView;
 
 import com.davidnardya.upractice.R;
+import com.davidnardya.upractice.adapters.MainAdapter;
 import com.davidnardya.upractice.pojo.Plan;
+import com.davidnardya.upractice.viewmodel.PlanListViewModel;
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.FirebaseFirestore;
 
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
 
     private RecyclerView planView;
+    private PlanListViewModel planListViewModel;
+    private MainAdapter adapter;
 
 
     private FirebaseFirestore dataBase = FirebaseFirestore.getInstance();
@@ -39,6 +48,12 @@ public class MainActivity extends AppCompatActivity {
 
         logOutBtn = findViewById(R.id.log_out_btn);
         userNameDisplay = findViewById(R.id.welcome_message_text_view);
+        planView = findViewById(R.id.plans_recycler_view);
+
+        planView.setLayoutManager(new LinearLayoutManager(this, RecyclerView.VERTICAL, false));
+        adapter = new MainAdapter();
+
+        planView.setAdapter(adapter);
 
         logOutBtn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -53,6 +68,15 @@ public class MainActivity extends AppCompatActivity {
         signInAccount = GoogleSignIn.getLastSignedInAccount(this);
         welcomeText = "Welcome, " + signInAccount.getDisplayName();
         userNameDisplay.setText(welcomeText);
+
+        planListViewModel = new ViewModelProvider(this).get(PlanListViewModel.class);
+        planListViewModel.getPlanListModelData().observe(this, new Observer<List<Plan>>() {
+            @Override
+            public void onChanged(List<Plan> planList) {
+                adapter.setPlanList(planList);
+                adapter.notifyDataSetChanged();
+            }
+        });
 
     }
 
--- app/src/main/java/com/davidnardya/upractice/adapters/MainAdapter.java
+++ app/src/main/java/com/davidnardya/upractice/adapters/MainAdapter.java
@@ -1,32 +1,66 @@
 package com.davidnardya.upractice.adapters;
 
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.davidnardya.upractice.R;
+import com.davidnardya.upractice.pojo.Plan;
+import com.davidnardya.upractice.viewmodel.PlanListViewModel;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import java.util.List;
+
 public class MainAdapter extends RecyclerView.Adapter<MainAdapter.MainViewHolder> {
+
+    private List<Plan> planList;
+
+    public void setPlanList(List<Plan> planList) {
+        this.planList = planList;
+    }
 
     @NonNull
     @Override
     public MainViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.single_plan_cell, parent, false);
+        return new MainViewHolder(view);
     }
 
     @Override
     public void onBindViewHolder(@NonNull MainViewHolder holder, int position) {
-
+        final Plan plan = planList.get(position);
+        holder.planName.setText(plan.getPlanName());
+        //TODO write logic of progress bar and change parameter according to logic
+        holder.planProgress.setProgress(50);
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        if(planList == null){
+            return 0;
+        } else {
+            return planList.size();
+        }
+
     }
 
     public class MainViewHolder extends RecyclerView.ViewHolder {
+
+        private TextView planName;
+        private ProgressBar planProgress;
+
         public MainViewHolder(@NonNull View itemView) {
             super(itemView);
+            planName = itemView.findViewById(R.id.single_cell_plan_name_text_view);
+            planProgress = itemView.findViewById(R.id.plan_progress_bar);
         }
+
+
     }
 }
