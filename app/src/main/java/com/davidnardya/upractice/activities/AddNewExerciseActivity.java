80df9ffdbc7f9ef01630c4b94053e7abdf5a99ac
davidnardya@gmail.com <`1q`1q~!Q>
2020-07-01 21:32:08 +0300

    Added time picker in new exercise activity

--- app/src/main/java/com/davidnardya/upractice/activities/AddNewExerciseActivity.java
+++ app/src/main/java/com/davidnardya/upractice/activities/AddNewExerciseActivity.java
@@ -3,10 +3,14 @@ package com.davidnardya.upractice.activities;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.TimePickerDialog;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TimePicker;
 
 import com.davidnardya.upractice.R;
 import com.davidnardya.upractice.pojo.Exercise;
@@ -17,6 +21,7 @@ import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.firestore.FirebaseFirestore;
 
 import java.sql.Timestamp;
+import java.util.Calendar;
 
 
 public class AddNewExerciseActivity extends AppCompatActivity {
@@ -25,6 +30,7 @@ public class AddNewExerciseActivity extends AppCompatActivity {
     FloatingActionButton finishExerciseFAB;
     String ExerciseName, ExerciseDescription;
     ExerciseStatus exerciseStatus = ExerciseStatus.NOT_STARTED;
+    Button pickTimeButton;
 
     String userID = FirebaseAuth.getInstance().getCurrentUser().getUid();
     FirebaseFirestore dataBase = FirebaseFirestore.getInstance();
@@ -47,11 +53,29 @@ public class AddNewExerciseActivity extends AppCompatActivity {
         newExerciseName = findViewById(R.id.new_exercise_name_edit_text);
         newExerciseDescription = findViewById(R.id.new_exercise_description_edit_text);
         finishExerciseFAB = findViewById(R.id.finish_exercise_fab);
+        pickTimeButton = findViewById(R.id.exercise_set_time_button);
 
         finishExerciseFAB.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 createNewExercise();
+            }
+        });
+
+        Calendar calendar = Calendar.getInstance();
+        final int currentHour = calendar.get(Calendar.HOUR_OF_DAY);
+        final int currentMinute = calendar.get(Calendar.MINUTE);
+        pickTimeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                TimePickerDialog timePickerDialog = new TimePickerDialog(AddNewExerciseActivity.this, new TimePickerDialog.OnTimeSetListener() {
+                    @Override
+                    public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+                        String time = "" + hourOfDay + ":" + minute;
+                        pickTimeButton.setText(time);
+                    }
+                },currentHour,currentMinute,android.text.format.DateFormat.is24HourFormat(AddNewExerciseActivity.this));
+                timePickerDialog.show();
             }
         });
 
