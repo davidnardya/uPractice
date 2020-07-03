d7217e6e1a6e4f359925a56ed1fff8ca06a05b43
davidnardya@gmail.com <`1q`1q~!Q>
2020-07-03 17:43:48 +0300

    Add new exerice activity: Updated time picker to view time by simple HH:mm format. Added date picker.

--- app/src/main/java/com/davidnardya/upractice/activities/AddNewExerciseActivity.java
+++ app/src/main/java/com/davidnardya/upractice/activities/AddNewExerciseActivity.java
@@ -3,12 +3,14 @@ package com.davidnardya.upractice.activities;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DatePickerDialog;
 import android.app.TimePickerDialog;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.TimePicker;
 
@@ -21,7 +23,10 @@ import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.firestore.FirebaseFirestore;
 
 import java.sql.Timestamp;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
+import java.util.Date;
 
 
 public class AddNewExerciseActivity extends AppCompatActivity {
@@ -30,7 +35,7 @@ public class AddNewExerciseActivity extends AppCompatActivity {
     FloatingActionButton finishExerciseFAB;
     String ExerciseName, ExerciseDescription;
     ExerciseStatus exerciseStatus = ExerciseStatus.NOT_STARTED;
-    Button pickTimeButton;
+    Button pickTimeButton, pickDateButton;
 
     String userID = FirebaseAuth.getInstance().getCurrentUser().getUid();
     FirebaseFirestore dataBase = FirebaseFirestore.getInstance();
@@ -54,6 +59,7 @@ public class AddNewExerciseActivity extends AppCompatActivity {
         newExerciseDescription = findViewById(R.id.new_exercise_description_edit_text);
         finishExerciseFAB = findViewById(R.id.finish_exercise_fab);
         pickTimeButton = findViewById(R.id.exercise_set_time_button);
+        pickDateButton = findViewById(R.id.exercise_set_date_button);
 
         finishExerciseFAB.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -65,17 +71,42 @@ public class AddNewExerciseActivity extends AppCompatActivity {
         Calendar calendar = Calendar.getInstance();
         final int currentHour = calendar.get(Calendar.HOUR_OF_DAY);
         final int currentMinute = calendar.get(Calendar.MINUTE);
+        final int currentDay = calendar.get(Calendar.DAY_OF_MONTH);
+        final int currentMonth = calendar.get(Calendar.MONTH);
+        final int currentYear = calendar.get(Calendar.YEAR);
         pickTimeButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 TimePickerDialog timePickerDialog = new TimePickerDialog(AddNewExerciseActivity.this, new TimePickerDialog.OnTimeSetListener() {
                     @Override
                     public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
-                        String time = "" + hourOfDay + ":" + minute;
+                        Calendar c = Calendar.getInstance();
+                        c.set(Calendar.HOUR_OF_DAY, hourOfDay);
+                        c.set(Calendar.MINUTE, minute);
+                        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("HH:mm");
+                        String time = simpleDateFormat.format(c.getTime());
                         pickTimeButton.setText(time);
                     }
                 },currentHour,currentMinute,android.text.format.DateFormat.is24HourFormat(AddNewExerciseActivity.this));
                 timePickerDialog.show();
+            }
+        });
+
+        pickDateButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DatePickerDialog datePickerDialog = new DatePickerDialog(AddNewExerciseActivity.this, new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                        Calendar c = Calendar.getInstance();
+                        c.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+                        c.set(Calendar.MONTH, month);
+                        c.set(Calendar.YEAR, year);
+                        String date = DateFormat.getDateInstance(DateFormat.FULL).format(c.getTime());
+                        pickDateButton.setText(date);
+                    }
+                },currentYear,currentMonth,currentDay);
+                datePickerDialog.show();
             }
         });
 
